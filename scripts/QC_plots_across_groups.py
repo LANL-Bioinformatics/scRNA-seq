f2a02648e5e2e81cf694c1a33919197f2a470258
MarthaDix <36462809+MarthaDix@users.noreply.github.com>
2024-10-14 11:04:21 -0600

    Aesthetic graph fixes and new example pre/post processing fixes (#10)
    * New normalization and cell typing added
    
    * Post cell type analysis files
    
    * parameter additions
    
    * code optimization
    
    * bug fixes and fixed issue #6
    
    * git ignore
    
    * testing fixes
    
    * file name fix
    
    * print fix
    
    * remove old env
    
    * Add description of parameters to readme and edited example
    
    * file name change
    
    * file read fix
    
    * Aesthetics graph changes and new example pre/post processing scripts
    
    * update documented Dockerfile. small fix writing out csv
    
    ---------
    
    Co-authored-by: Martha Katherine Dix <mdix@canopus.m.global>
    Co-authored-by: Andre Watson <apwat@uoregon.edu>

--- scripts/QC_plots_across_groups.py
+++ scripts/QC_plots_across_groups.py
@@ -45,7 +45,7 @@ def qc_plots_across_groups(adata, out_dir):
         counter_obj = Counter(sample_id_list)
         x_keys = list(counter_obj.keys())
         x_values = list(counter_obj.values())
-        fig = plt.figure(figsize = (25, 10))
+        fig = plt.figure()
         plt.xlabel('Number of Cells Per Sample')
         plt.bar(x_keys, x_values, color ='maroon')
         plt.savefig(os.path.join(out_dir, outcome + "_cells_per_sample.png"))
@@ -56,7 +56,7 @@ def qc_plots_across_groups(adata, out_dir):
         for i in x_values:
             x_avg.append(i/len(sample_id_list))
 
-        fig = plt.figure(figsize = (25, 10))
+        fig = plt.figure()
         plt.bar(x_keys, x_avg, color ='purple')
         plt.xlabel('Percent Cells Per Sample')
         plt.savefig(os.path.join(out_dir,outcome + "_percent_cells_per_sample.png"))
@@ -64,18 +64,19 @@ def qc_plots_across_groups(adata, out_dir):
         df = pd.DataFrame({'Sample List': x_keys, 'Cells per Sample': x_values, 'Fraction Cells per Sample': x_avg})
         df = df.set_index("Sample List")
         print(df)
-    
+        df.to_csv(os.path.join(out_dir, outcome+'_cell_count_summary.csv')) 
+
     # QC plots comparing all samples
     with plt.rc_context():
-        scanpy.pl.violin(adata, 'total_counts', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False) 
+        scanpy.pl.violin(adata, 'total_counts', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False, rotation=90) 
         plt.savefig(os.path.join(out_dir,"counts_by_sample.png"))
 
     with plt.rc_context():
-        scanpy.pl.violin(adata, 'n_genes_by_counts', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False)
+        scanpy.pl.violin(adata, 'n_genes_by_counts', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False, rotation=90)
         plt.savefig(os.path.join(out_dir, "number_genes_by_sample.png"))
 
     with plt.rc_context():
-        scanpy.pl.violin(adata, 'pct_counts_mt', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False)
+        scanpy.pl.violin(adata, 'pct_counts_mt', jitter=0.4, groupby='sample_id',  multi_panel=True, show=False, rotation=90)
         plt.savefig(os.path.join(out_dir, "percent_mito_by_sample.png"))
 
 
